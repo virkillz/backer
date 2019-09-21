55da357b54a11c701d4e8cbfdbdd03a0dc76697d
virkonomy <arif@tokenomy.com>
2019-09-21 15:40:31 +0700

    Add badge seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,10 +1,23 @@
 alias Backer.Repo
 alias Backer.Account.User
+alias Backer.Gamification.Badge
 
-  Repo.insert! %User{
-    fullname: "Joe Admin",
-    username: "administrator",
-    password_hash: "$2b$12$8As.fIX4fQsbZuhcIhKr7OU3fqxaaPsfYuFZ/S6fUEDd2HDkzN.Tu",
-    avatar: "/images/default.png",
-    role: "administrator"    
-  }
+Repo.insert!(%User{
+  fullname: "Joe Admin",
+  username: "administrator",
+  password_hash: "$2b$12$8As.fIX4fQsbZuhcIhKr7OU3fqxaaPsfYuFZ/S6fUEDd2HDkzN.Tu",
+  avatar: "/images/default.png",
+  role: "administrator"
+})
+
+Repo.insert!(%Badge{
+  title: "Light Backer"
+})
+
+Repo.insert!(%Badge{
+  title: "Heavy Backer"
+})
+
+Repo.insert!(%Badge{
+  title: "Ultra Backer"
+})
