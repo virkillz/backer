6e29e78ed1a3151f3ac73c6b46f2980c714c89b7
virkonomy <arif@tokenomy.com>
2019-10-05 13:51:26 +0700

    remove duplicated content module

--- lib/backer/content/content.ex
+++ lib/backer/content/content.ex
@@ -10,6 +10,7 @@ defmodule Backer.Content do
   alias Backer.Finance.Donation
   alias Backer.Account.Donee
   alias Backer.Account.Backer, as: Backerz
+  alias Backer.Content.Notification
   alias Backer.Content.PostComment
   alias Backer.Content.PostLike
   alias Backer.Content.PCommentLike
@@ -1200,5 +1201,99 @@ defmodule Backer.Content do
   """
   def change_p_comment_like(%PCommentLike{} = p_comment_like) do
     PCommentLike.changeset(p_comment_like, %{})
+  end
+
+  @doc """
+  Returns the list of notifications.
+
+  ## Examples
+
+      iex> list_notifications()
+      [%Notification{}, ...]
+
+  """
+  def list_notifications do
+    Repo.all(Notification)
+  end
+
+  @doc """
+  Gets a single notification.
+
+  Raises `Ecto.NoResultsError` if the Notification does not exist.
+
+  ## Examples
+
+      iex> get_notification!(123)
+      %Notification{}
+
+      iex> get_notification!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_notification!(id), do: Repo.get!(Notification, id)
+
+  @doc """
+  Creates a notification.
+
+  ## Examples
+
+      iex> create_notification(%{field: value})
+      {:ok, %Notification{}}
+
+      iex> create_notification(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_notification(attrs \\ %{}) do
+    %Notification{}
+    |> Notification.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a notification.
+
+  ## Examples
+
+      iex> update_notification(notification, %{field: new_value})
+      {:ok, %Notification{}}
+
+      iex> update_notification(notification, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_notification(%Notification{} = notification, attrs) do
+    notification
+    |> Notification.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Notification.
+
+  ## Examples
+
+      iex> delete_notification(notification)
+      {:ok, %Notification{}}
+
+      iex> delete_notification(notification)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_notification(%Notification{} = notification) do
+    Repo.delete(notification)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking notification changes.
+
+  ## Examples
+
+      iex> change_notification(notification)
+      %Ecto.Changeset{source: %Notification{}}
+
+  """
+  def change_notification(%Notification{} = notification) do
+    Notification.changeset(notification, %{})
   end
 end
