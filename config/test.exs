fe93b693e75032c0355a6d516340714d7b968ed6
virkonomy <arif@tokenomy.com>
2019-10-25 11:47:36 +0700

    try fix test

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :backer, Backer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
+  username: "indodax",
   password: "postgres",
   database: "backer_test",
   hostname: "localhost",
