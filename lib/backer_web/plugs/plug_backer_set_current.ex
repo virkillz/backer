9e6c4f0173587bcdd6e60a22b3129f027191c1bb
virkonomy <arif@tokenomy.com>
2019-11-03 15:18:18 +0700

    fix plug

--- lib/backer_web/plugs/plug_backer_set_current.ex
+++ lib/backer_web/plugs/plug_backer_set_current.ex
@@ -12,14 +12,26 @@ defmodule BackerWeb.Plugs.SetCurrentBacker do
     backer_id = Plug.Conn.get_session(conn, :current_backer_id)
     current_backer = Account.get_backer_preload_donee(backer_id)
 
-    if is_nil(current_backer) do
-      conn
-      |> assign(:current_backer, nil)
-      |> assign(:backer_signed_in?, false)
-    else
-      conn
-      |> assign(:current_backer, current_backer)
-      |> assign(:backer_signed_in?, true)
+    # if is_nil(current_backer) do
+    #   conn
+    #   |> assign(:current_backer, nil)
+    #   |> assign(:backer_signed_in?, false)
+    # else
+    #   conn
+    #   |> assign(:current_backer, current_backer)
+    #   |> assign(:backer_signed_in?, true)
+    # end
+
+    cond do
+      current_backer = backer_id && Account.get_backer_preload_donee(backer_id) ->
+        conn
+        |> assign(:current_backer, current_backer)
+        |> assign(:backer_signed_in?, true)
+
+      true ->
+        conn
+        |> assign(:current_backer, nil)
+        |> assign(:backer_signed_in?, false)
     end
 
     # cond do
